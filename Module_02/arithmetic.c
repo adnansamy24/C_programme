6de34b366dd180e9befa41aa0256670b9a2df92b
Adnan Samy <118908917+adnansamy24@users.noreply.github.com>
2023-04-07 16:42:18 +0600

    starting

---
+++ Module_02/arithmetic.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+
+    int a =3,b =20;
+    int total =b % a;
+    printf("%d", total);
+    return 0;
+}
---
+++ Module_02/ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int prem;
+    scanf("%d",&prem);
+
+
+    if (prem<=10)
+    {
+        printf("bia korbo");
+    }else
+    {
+       printf("bia korbo na");
+    }
+    return 0;
+}
